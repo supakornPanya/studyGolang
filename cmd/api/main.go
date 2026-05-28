5c0ea69d68d90cb9c37f045b8da61107c89c5ca0
zoom beryl8 <supakronnn55@gmail.com>
2026-05-28 11:19:51 +0700

    feat: secure existing item routes and register register/login routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"net/http"
+	"study-golang-backend/internal/auth"
 	"study-golang-backend/internal/item"
+	"study-golang-backend/internal/user"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	jwtSecret := []byte("my_super_secret_dev_key")
+
 	//Init Gin router
 	r := gin.Default()
 
@@ -18,12 +22,21 @@ func main() {
 		})
 	})
 
+	// Setup User authentication
+	userRepo := user.NewMemoryRepository()
+	userHandler := user.NewHandler(userRepo, jwtSecret)
+	userHandler.RegisterRouter(r.Group(""))
+
 	// Initial example Data
 	itemRepo := item.NewMemoryRepository()
 	// Initial Handler & inject initial Data(itemRepo)
 	itemHandler := item.NewHandler(itemRepo)
-	// Register Router
-	itemHandler.RegisterRouter(r.Group(""))
+
+	// Register Router secured by AuthMiddleware
+	protected := r.Group("")
+	protected.Use(auth.AuthMiddleware(jwtSecret))
+	itemHandler.RegisterRouter(protected)
 
 	r.Run(":8080")
 }
+
